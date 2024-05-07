55293d8310772981b61e1a7bcae0e83b58c6b9c7
damirusso95 <damirusso95@gmail.com>
2024-05-07 15:30:04 +0200

    first commit

---
+++ function.js
@@ -0,0 +1,18 @@
+// CONSEGNA
+// Ricreate un carosello, ma questa volta usando un array di oggeti.
+// Ogni slide è rappresentata da un oggetto contenente:
+// percorso immagine
+// titolo
+// descrizione
+// :roccia: MILESTONE #0
+// Come nel primo carosello realizzato, focalizziamoci prima sulla creazione del markup statico:
+//  costruiamo il container e inseriamo l'immagine grande in modo da poter stilizzare lo slider.
+// :roccia: MILESTONE #1
+// Ora rimuoviamo i contenuti statici dall'HTML.
+// Costruiamo noi delle slide da aggiungere in pagina tramite un ciclo sul nostro array.
+// :roccia: MILESTONE #2
+// Al click dell'utente sulle frecce verso sinistra o destra, l'immagine attiva diventerà visibile e dovremo aggiungervi titolo e testo.
+// :roccia: MILESTONE #3
+// Aggiungere il ciclo infinito del carosello.
+//  Ovvero se l'immagine attiva è la prima e l'utente clicca la freccia verso destra,
+//   la slide che deve attivarsi sarà l'ultima e viceversa per l'ultima slide se l'utente clicca la freccia verso sinistra.
